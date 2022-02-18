a1e42890c8af56ce62a7034f729500be1a062f77
dhkim <dhkimxx@naver.com>
2022-02-18 13:44:24 +0900

    2022-02-18

--- 11047.py
+++ 11047.py
@@ -1,15 +1,11 @@
 N,K = map(int, input().split())
-result = 0
 coin = []
 for i in range(N):
     coin.append(int(input().strip()))
 coin.sort(reverse=True)
-i = 0
-while True:
-    if coin[i] < K:
-        result = result + K // coin[i]
-        K = K % coin[i]
-    i += 1
-    if i >= N:
-        break
+result = 0
+for i in range(N):
+    if coin[i] <= K:
+        result += K // coin[i]
+        K %= coin[i]
 print(result)
