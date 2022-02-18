b59f72bfa633d5085a202333e404a316479d8165
dhkimxx <dhkimxx@naver.com>
2022-02-18 20:58:49 +0900

    2022-02-18

--- 1931.py
+++ 1931.py
@@ -3,19 +3,20 @@ if N == 1:
     print(1)
     exit()
 timetable = [[*map(int, input().split())] for _ in range(N)]
-timetable.sort()
+timetable.sort(key=lambda x: [x[1], x[0]])
 max = 0
+end = 0
+count = 0
+test = []
 for i in range(N):
-    end = timetable[i][1]
-    count = 1
-    test = [timetable[i]]
-    for j in range(i + 1, N):
-        if timetable[j][0] < end:
-            continue
-        else:
-            end = timetable[j][1]
-            count += 1
-            test.append(timetable[j])
+    if timetable[i][0] == timetable[i][1]:
+        end = timetable[i][1]
+        count += 1
+        test.append(timetable[i])
+    elif timetable[i][0] >= end:
+        end = timetable[i][1]
+        count += 1
+        test.append(timetable[i])
     if count > max:
         max = count
 print(max)
