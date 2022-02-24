37825c51369980fdfbc4597659f7f98846dc26f5
dhkim <dhkimxx@naver.com>
2022-02-24 15:36:51 +0900

    2022-02-24

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-temtLst = lst.copy()
-n = 0
-for i in range(len(lst)):
-    lst = temtLst
-    for j in range(len(lst)):
-        lst[i], lst[j] = lst[j], lst[i]
-        n += 1
-        print(lst,n)
+lst[1:3] = 0
+print(lst)
