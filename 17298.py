1caa4c3b139ffa9503dfc869f2bbe92203a75a8d
dhkimxx <dhkimxx@naver.com>
2022-02-25 16:12:39 +0900

    2022-02-25

--- 17298.py
+++ 17298.py
@@ -1,18 +1,11 @@
 import sys
 N = int(sys.stdin.readline())
-A = list(sys.stdin.readline().split())
-stack = [A[0]]
-NGE = [-1 for i in range(N)]
-j = 0
-for i in range(N):
-    if stack[-1] < A[i]:
-        for k in range(j, i):
-            if A[k] < A[i]:
-                NGE[k] = A[i]
-        stack = [A[i]]
-        j = i
-    else:
-        stack.append(A[i])
-
+A = list(map(int, sys.stdin.readline().split()))
+NGE = [-1] * N
+index = []
+for i in range(0, N):
+    while len(index) != 0 and A[index[-1]] < A[i]:
+        NGE[index.pop()] = A[i]
+    index.append(i)
 for n in range(len(NGE)):
     print(NGE[n], end=' ')
