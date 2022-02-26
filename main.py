5948b933a9d4c74a69ec3d34367bdb0e04365b8a
dhkimxx <dhkimxx@naver.com>
2022-02-26 18:18:50 +0900

    2022-02-26

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-import sys
-
-N = int(sys.stdin.readline())
-card = []
-for n in range(1, N + 1):
-    card.append(n)
-while len(card) > 1:
-    card.pop(0)
-    card.append(card.pop(0))
-print(card)
+A, B = map(int, input().split())
+print((A + B) * (A - B))
