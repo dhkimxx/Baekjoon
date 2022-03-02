1bbca120b59051415a93c048333d72cc7e229837
dhkimxx <dhkimxx@naver.com>
2022-03-02 19:56:24 +0900

    2022-03-02

--- 12104.py
+++ 12104.py
@@ -1,12 +1,50 @@
-#https://blog.naver.com/b1327e/222639590710
-#12104번 순환 순열
-import sys
-from collections import deque
-A = deque(sys.stdin.readline().rstrip())
-B = deque(sys.stdin.readline().rstrip())
+"""
+#slicing 사용
+A, B = input(), input()
+newA = A + A[:-1]
+Len = len(B)
 count = 0
-for n in range(len(B)):
-    if A == B:
-        count += 1
-    B.rotate(-1)
+for i in range(Len):
+  if newA[i:i + Len] == B:
+    count += 1
 print(count)
+"""
+#KMP 사용
+import sys
+
+
+def makeTable(pattern):
+    patternSize = len(pattern)
+    table = [0] * patternSize
+    j = 0
+    for i in range(1, patternSize):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = table[j - 1]
+        if pattern[i] == pattern[j]:
+            j += 1
+            table[i] = j
+    return table
+
+
+def KMP(parent, pattern):
+    table = makeTable(pattern)
+    parentSize = len(parent)
+    patternSize = len(pattern)
+    count = 0
+    j = 0
+    for i in range(0, parentSize):
+        while j > 0 and parent[i] != pattern[j]:
+            j = table[j - 1]
+        if parent[i] == pattern[j]:
+            if j == patternSize - 1:
+                count += 1
+                j = table[j]
+            else:
+                j += 1
+    return count
+
+
+A = sys.stdin.readline().rstrip()
+B = sys.stdin.readline().rstrip()
+print(KMP(A + A[:-1], B))
+
