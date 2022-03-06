5b4da9c743881bcffac487bb64bb767f038b1c23
dhkimxx <dhkimxx@naver.com>
2022-03-06 16:06:34 +0900

    2022-03-06

---
+++ 1300.py
@@ -0,0 +1,15 @@
+N, K = int(input()), int(input())
+start = 1
+end = N * N
+result = 0
+while end - start >= 0:
+    mid = (start + end) // 2
+    count = 0
+    for i in range(1, N + 1):
+        count += min(mid / i, N)
+    if count < K:
+        start = mid + 1
+    if count > K:
+        result = mid
+        end = mid - 1
+print(result)
