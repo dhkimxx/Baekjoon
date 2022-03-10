82ffe3716f8c53afd777ebb16731fd5fc063c8dd
dhkimxx <dhkimxx@naver.com>
2022-03-10 20:12:07 +0900

    2022-03-10

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-for i in range(1,10):
-    for j in range(1,10):
-        if i*j < 10:
-            print(" ",end="")
-        print(i*j,end=" ")
-    print()
+firstYearm, secondYear = list(input().split()), list(input(). split())
+
