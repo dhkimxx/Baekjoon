6e58b59cbdc8be520938c9881d17c2994fd09ea9
dhkimxx <dhkimxx@naver.com>
2022-03-10 21:28:10 +0900

    2022-03-10

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-firstYear, secondYear = list(input().split()), list(input(). split())
-if firstYear[0] == 'AD' and secondYear[0] == 'AD':
-    print(abs(int(firstYear[1]) - int(secondYear[1])))
-if firstYear[1] == 'BC' and secondYear[0] == 'AD':
-    print(int(firstYear[0]) + int(secondYear[1]) - 1)
-if firstYear[0] == 'AD' and secondYear[1] == 'BC':
-    print(int(firstYear[1]) + int(secondYear[0]) - 1)
-if firstYear[1] == 'BC' and secondYear[1] == 'BC':
-    print(abs(int(firstYear[0]) - int(secondYear[0])))
+S = int(input())
+cnt = 0
+Sum = 0
+while 1:
+    cnt += 1
+    Sum += cnt
+    if Sum > S:
+        print(cnt - 1)
+        exit()
