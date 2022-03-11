b760d3a2da11e4b865ed20fc7d514336d3c7b369
dhkimxx <dhkimxx@naver.com>
2022-03-11 21:19:32 +0900

    2022-03-11

---
+++ 14002.py
@@ -0,0 +1,10 @@
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+A.reverse()
+dp = [1] * N
+for i in range(0, N):
+    for j in range(0, i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
