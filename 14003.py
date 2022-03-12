5ead7a5adbb820795534b268d9f8e186c18d46df
dhkimxx <dhkimxx@naver.com>
2022-03-12 21:46:04 +0900

    2022-03-12

---
+++ 14003.py
@@ -0,0 +1,16 @@
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+LIS = []
+maxLength = 0
+for i in range(0, N):
+    LIS.append([A[i]])
+    for j in range(0, i):
+        if A[i] > A[j] and len(LIS[i]) < len(LIS[j]) + 1:
+            LIS[i] = LIS[j] + [A[i]]
+    if len(LIS[i]) > maxLength:
+        maxLIS = LIS[i]
+        maxLength = len(LIS[i])
+print(maxLength)
+for lis in maxLIS:
+    print(lis, end=" ")
--- main.py
+++
@@ -1,9 +0,0 @@
-N = input()
-result = 0
-for n in range(len(N)):
-    if ord(N[n]) <= 57:
-        result += N[n] * 16 ** (len(N) - n - 1)
-    else:
-        result += (ord(N[n]) - 55) * 16 ** (len(N) - n - 1)
-print(result)
-print(int(N, 16))
