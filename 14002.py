b990591aa405c0c2f144ead8652ac3cf624a2290
dhkimxx <dhkimxx@naver.com>
2022-03-12 18:21:32 +0900

    2022-03-12

--- 14002.py
+++ 14002.py
@@ -1,10 +1,16 @@
 import sys
 N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
-A.reverse()
-dp = [1] * N
+LIS = []
+maxLength = 0
 for i in range(0, N):
+    LIS.append([A[i]])
     for j in range(0, i):
-        if A[i] > A[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
-print(max(dp))
+        if A[i] > A[j] and len(LIS[i]) < len(LIS[j]) + 1:
+            LIS[i] = LIS[j] + [A[i]]
+    if len(LIS[i]) > maxLength:
+        maxLengthLIS = LIS[i]
+        maxLength = len(LIS[i])
+print(maxLength)
+for lis in maxLengthLIS:
+    print(lis, end=" ")
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-a, b = map(int, input().split())
-print(a // b, end='')
-if a % b:
-    i = 0
-    print('.', end='')
-    while a % b and i < 1000:
-        a = a % b * 10
-        i += 1
-        print(a // b, end='')
+N = input()
+result = 0
+for n in range(len(N)):
+    if ord(N[n]) <= 57:
+        result += N[n] * 16 ** (len(N) - n - 1)
+    else:
+        result += (ord(N[n]) - 55) * 16 ** (len(N) - n - 1)
+print(result)
+print(int(N, 16))
