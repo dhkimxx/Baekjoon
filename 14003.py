bce34e3316a2cff63244b3a3fe0cfb35cc6819d1
dhkimxx <dhkimxx@naver.com>
2022-03-14 22:03:01 +0900

    2022-03-14

--- 14003.py
+++ 14003.py
@@ -1,13 +1,13 @@
 import sys
 N = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-LIS = [arr[0]]
+A = list(map(int, sys.stdin.readline().split()))
+LIS = [A[0]]
 DP = [1]*N
 maxDP = 0
 maxDP_idx = 0
 for n in range(1, N):
-    if LIS[-1] < arr[n]:
-        LIS.append(arr[n])
+    if LIS[-1] < A[n]:
+        LIS.append(A[n])
         DP[n] = len(LIS)
     else:
         start = 0
@@ -15,12 +15,12 @@ for n in range(1, N):
         LIS_idx = 0
         while end - start >= 0:
             mid = (start + end) // 2
-            if LIS[mid] < arr[n]:
+            if LIS[mid] < A[n]:
                 LIS_idx = mid + 1
                 start = mid + 1
             else:
                 end = mid - 1
-        LIS[LIS_idx] = arr[n]
+        LIS[LIS_idx] = A[n]
         DP[n] = LIS_idx + 1
     if DP[n] > maxDP:
         maxDP_idx = n
@@ -28,10 +28,10 @@ for n in range(1, N):
 
 print(len(LIS))
 
-resultLIS = [arr[maxDP_idx]]
+resultLIS = [A[maxDP_idx]]
 for n in range(maxDP_idx, -1, -1):
-    if DP[n] == DP[maxDP_idx] - 1 and arr[n] < arr[maxDP_idx]:
-        resultLIS.append(arr[n])
+    if DP[n] == DP[maxDP_idx] - 1 and A[n] < A[maxDP_idx]:
+        resultLIS.append(A[n])
         maxDP_idx = n
 
 print(' '.join(map(str, reversed(resultLIS))))
