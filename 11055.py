4932ec40746d87057183008af97ba706029bacc6
dhkimxx <dhkimxx@naver.com>
2022-03-15 21:16:23 +0900

    2022-03-15

--- 11055.py
+++ 11055.py
@@ -6,10 +6,10 @@ maxSum = A[0]
 for i in range(0, N):
     LIS.append([A[i]])
     for j in range(i):
-        if A[j] < A[i] and len(LIS[i]) < len(LIS[j]) + 1:
+        if A[j] < A[i] and sum(LIS[j]) + A[i] > sum(LIS[i]):
             LIS[i] = LIS[j] + [A[i]]
     sumOfLIS = sum(LIS[i])
     if sumOfLIS > maxSum:
         maxSum = sumOfLIS
         maxLIS = LIS[i]
-print(maxSum, LIS)
+print(maxSum)
