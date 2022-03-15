db3a4eb1bd4326bdc2a0d5a4fd2f3a87b65f159e
dhkimxx <dhkimxx@naver.com>
2022-03-15 21:55:23 +0900

    2022-03-15

--- main.py
+++
@@ -1,8 +0,0 @@
-N, F = int(input()), int(input())
-result = 100
-for n in range(N // 100 * 100, N // 100 * 100 + 100):
-    if n % F == 0 and n % 100 < result:
-        result = n % 100
-if result < 10:
-    print(0,end="")
-print(result)
