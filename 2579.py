342b5a48cba9063ac571512942352d7ac93667cb
dhkimxx <dhkimxx@naver.com>
2022-03-19 21:13:12 +0900

    2022-03-19

--- 2579.py
+++ 2579.py
@@ -1,9 +1,14 @@
 import sys
+
 N = int(sys.stdin.readline())
 score = []
 for i in range(N):
     score.append(int(sys.stdin.readline()))
 dp = [0] * N
-for i in range(N):
-    point = 0
-    for j in range(i):
+dp[0] = score[0]
+if N >= 2:
+    dp[1] = score[0] + score[1]
+if N >= 3:
+    for i in range(2, N):
+        dp[i] = max(dp[i - 2] + score[i], dp[i - 3] + score[i - 1] + score[i])
+print(dp[-1])
