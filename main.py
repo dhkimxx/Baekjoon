04c649265670b43eec1abd07fd72054f4308539b
dhkimxx <dhkimxx@naver.com>
2022-03-21 15:51:49 +0900

    2022-03-21

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-L, P = map(int, input().split())
-participant = list(map(int, input().split()))
-for n in participant:
-    print(n - L * P,end=" ")
+N = input()
+startBinary = ["0","1","10","11","100","101","110","111"]
+binary = ["000","001","010","011","100","101","110","111"]
+print(startBinary[int(N[0])], end="")
+for n in N[1:]:
+    print(binary[int(n)], end="")
