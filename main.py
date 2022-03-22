2aacc9dfc6fa49ba41c76c80c34b564c764316ef
dhkimxx <dhkimxx@naver.com>
2022-03-22 19:01:24 +0900

    2022-03-22

--- main.py
+++
@@ -1,6 +0,0 @@
-N = input()
-startBinary = ["0","1","10","11","100","101","110","111"]
-binary = ["000","001","010","011","100","101","110","111"]
-print(startBinary[int(N[0])], end="")
-for n in N[1:]:
-    print(binary[int(n)], end="")
