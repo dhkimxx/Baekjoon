420ba2fc30f59daaa07772f6aaad3b13344a01a0
dhkimxx <dhkimxx@naver.com>
2022-03-23 22:21:53 +0900

    2022-03-23

--- 1655.py
+++ 1655.py
@@ -38,21 +38,17 @@ print(mid)
 for n in range(2, N + 1):
     x = int(sys.stdin.readline())
     if mid <= x:
-        if len(minHeap) - len(maxHeap) == 1:
-            heapq.heappush(minHeap, x)
+        heapq.heappush(minHeap, x)
+        if len(minHeap) - len(maxHeap) == 2:
             heapq.heappush(maxHeap, -mid)
             mid = heapq.heappop(minHeap)
-        else:
-            heapq.heappush(minHeap, x)
-
+    
     if mid > x:
-        if len(maxHeap) - len(minHeap) == 1:
-            heapq.heappush(maxHeap, -x)
+        heapq.heappush(maxHeap, -x)
+        if len(maxHeap) - len(minHeap) == 2:
             heapq.heappush(minHeap, mid)
             mid = -heapq.heappop(maxHeap)
-        else:
-            heapq.heappush(maxHeap, -x)
-
+            
     if len(maxHeap) <= len(minHeap):
         print(mid)
     if len(maxHeap) > len(minHeap):
