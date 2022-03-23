cb48427456bdf1410926d4b87c9add4be36beffd
dhkimxx <dhkimxx@naver.com>
2022-03-23 20:54:32 +0900

    2022-03-23

--- 2667.py
+++ 2667.py
@@ -1,3 +1,5 @@
+#DFS 이용
+
 import sys
 N = int(sys.stdin.readline())
 graph = []
@@ -26,3 +28,44 @@ for i in range(N):
 print(len(result))
 for r in sorted(result):
     print(r)
+#BFS 이용
+'''
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+graph = []
+for n in range(N):
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    cnt = 0
+    queue = deque([(x, y)])
+    graph[x][y] = 0
+    while queue:
+        x, y = queue.popleft()
+        cnt += 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= N or ny >= N:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+
+    return cnt
+
+result = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] != 0:
+            result.append(bfs(i, j))
+
+print(len(result))
+for r in sorted(result):
+    print(r)
+'''
