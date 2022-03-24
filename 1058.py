b448dad512b26294200e0babf1c287f945478fd5
dhkimxx <dhkimxx@naver.com>
2022-03-25 00:39:01 +0900

    2022-03-25

--- 1058.py
+++ 1058.py
@@ -1,14 +1,24 @@
+import copy
 import sys
 
 N = int(sys.stdin.readline())
 graph = []
 for n in range(N):
     graph.append(list(sys.stdin.readline().rstrip()))
+friends = []
+result = [0] * N
 
-count = [0] * N
 for c in range(N):
-    count[c] = graph[c].count('Y')
+    friends.append([])
     for a in range(N):
         if graph[c][a] == 'Y':
-            count[c] += graph[a].count('Y') - 1
-print(max(count))
+            friends[c].append(a)
+    result[c] = len(friends[c])
+
+for c in range(N):
+    for a_friends in friends[c]:
+        for b_friends in friends[a_friends]:
+            if b_friends != c and b_friends not in friends[c]:
+                result[c] += 1
+
+print(max(result))
