c0355077d5fe274bb910b166167bf83996f3466f
dhkimxx <dhkimxx@naver.com>
2022-03-25 18:27:18 +0900

    2022-03-25

--- 2458.py
+++ 2458.py
@@ -13,6 +13,7 @@ for k in range(1, N + 1):
     for i in range(1, N + 1):
         for j in range(1, N + 1):
             if i == j:
+                graph[i][j] = 1
                 continue
             if graph[i][k] == 1 and graph[k][j] == 1:
                 graph[i][j] = 1
@@ -23,6 +24,6 @@ for k in range(1, N + 1):
 
 result = 0
 for i in range(1, N + 1):
-    if graph[i][1:].count(0) == 1:
+    if graph[i][1:].count(0) == 0:
         result += 1
 print(result)
