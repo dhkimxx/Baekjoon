8d0aedb43d652bda35a62c9448576d243241c0ec
dhkimxx <dhkimxx@naver.com>
2022-03-25 18:25:49 +0900

    2022-03-25

---
+++ 2458.py
@@ -0,0 +1,28 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+graph = [[0] * (N + 1) for _ in range(N + 1)]
+visited = [[0] * (N + 1) for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a][b] = -1
+    graph[b][a] = 1
+
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            if i == j:
+                continue
+            if graph[i][k] == 1 and graph[k][j] == 1:
+                graph[i][j] = 1
+                graph[j][i] = -1
+            if graph[i][k] == -1 and graph[k][j] == -1:
+                graph[i][j] = -1
+                graph[j][i] = 1
+
+result = 0
+for i in range(1, N + 1):
+    if graph[i][1:].count(0) == 1:
+        result += 1
+print(result)
