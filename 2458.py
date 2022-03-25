78ca612dbce287ad0b6f82cd73c36635fd8785a0
dhkimxx <dhkimxx@naver.com>
2022-03-25 18:33:37 +0900

    2022-03-25

--- 2458.py
+++ 2458.py
@@ -1,8 +1,9 @@
 import sys
 
+INF = int(1e9)
+
 N, M = map(int, sys.stdin.readline().split())
-graph = [[0] * (N + 1) for _ in range(N + 1)]
-visited = [[0] * (N + 1) for _ in range(N + 1)]
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
 
 for _ in range(M):
     a, b = map(int, sys.stdin.readline().split())
@@ -13,7 +14,7 @@ for k in range(1, N + 1):
     for i in range(1, N + 1):
         for j in range(1, N + 1):
             if i == j:
-                graph[i][j] = 1
+                graph[i][j] = 0
                 continue
             if graph[i][k] == 1 and graph[k][j] == 1:
                 graph[i][j] = 1
@@ -24,6 +25,6 @@ for k in range(1, N + 1):
 
 result = 0
 for i in range(1, N + 1):
-    if graph[i][1:].count(0) == 0:
+    if graph[i][1:].count(INF) == 0:
         result += 1
 print(result)
