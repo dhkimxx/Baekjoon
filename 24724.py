b9d319d013cc0f80def44719d8bdbfa9cec78574
dhkimxx <dhkimxx@naver.com>
2022-03-26 19:06:31 +0900

    2022-03-26

---
+++ 24724.py
@@ -0,0 +1,7 @@
+for t in range(int(input())):
+    N = int(input())
+    A, B = map(int, input().split())
+    print(f'Material Management {t + 1}')
+    for n in range(N):
+        a, b = map(int, input().split())
+    print('Classification ---- End!')
