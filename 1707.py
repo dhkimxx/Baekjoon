00042d486aea123f19b1c3c8f0ce124d22fc86e9
dhkim <dhkimxx@naver.com>
2022-03-28 14:49:58 +0900

    2022-03-28

--- 1707.py
+++ 1707.py
@@ -1,8 +1,17 @@
 import sys
 
-def dfs(v, graph, visited):
-    visited[v] = 1
+def dfs(v, c):
+    visited[v] = c
+    if c == 1:
+        nc = 2
+    if c == 2:
+        nc = 1
 
+    for nv in graph[v]:
+        if visited[nv] == '0':
+            dfs(nv, nc)
+        elif visited[nv] != nc:
+            result = 'NO'
 
 K = int(sys.stdin.readline())
 for k in range(K):
@@ -14,7 +23,9 @@ for k in range(K):
         graph[u].append(v)
         graph[v].append(u)
 
+    check = 1
+    result = 'YES'
     for i in range(1, V + 1):
         if visited[i] == '0':
-            dfs(i)
-
+            dfs(i, check)
+    print(result)
