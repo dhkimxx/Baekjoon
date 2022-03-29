e9dc3eac647cdac28a935f8b94515a00e9a6ece5
dhkim <dhkimxx@naver.com>
2022-03-29 11:02:16 +0900

    2022-03-29

--- 2565.py
+++ 2565.py
@@ -1,19 +1,13 @@
 import sys
-import heapq
 
 N = int(sys.stdin.readline())
-heap = []
-A = []
-LIS = [1] * N
+A = [0] * 500
+LIS = [1] * 500
 for n in range(N):
     a, b = map(int, sys.stdin.readline().split())
-    heapq.heappush(heap,(a, b))
-for n in range(N):
-    A.append(heapq.heappop(heap)[1])
-
-
-for i in range(0, N):
+    A[a - 1] = b
+for i in range(0, 500):
     for j in range(0, i):
-        if A[i] > A[j]:
+        if A[i] > A[j] != 0:
             LIS[i] = max(LIS[j] + 1, LIS[i])
 print(N - max(LIS))
