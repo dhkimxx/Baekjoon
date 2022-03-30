4404de07d2051ab36dd9cf2a60cba5734de5ca8b
dhkimxx <dhkimxx@naver.com>
2022-03-30 23:32:18 +0900

    2022-03-30

--- 7662.py
+++ 7662.py
@@ -1,19 +1,36 @@
-import sys
+import heapq
 
-for _ in range(int(sys.stdin.readline())):
-    arr = []
-    for _ in range(int(sys.stdin.readline())):
-        Q, N = list(sys.stdin.readline().split())
+for _ in range(int(input())):
+    k = int(input())
+    max_heap = []
+    min_heap = []
+    visited = [False] * k
+
+    for key in range(k):
+        Q, N = input().split()
         if Q == 'I':
-            arr.append(int(N))
-
-        if Q == 'D' and len(arr) != 0:
-            arr.sort()
+            heapq.heappush(min_heap, (int(N), key))
+            heapq.heappush(max_heap, (-int(N), key))
+            visited[key] = True
+        if Q == 'D':
             if N == '1':
-                arr.pop()
+                while max_heap and not visited[max_heap[0][1]]:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    visited[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
             if N == '-1':
-                arr.pop(0)
-    if len(arr) == 0:
+                while min_heap and not visited[min_heap[0][1]]:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    visited[min_heap[0][1]] = False
+                    heapq.heappop(min_heap)
+
+        while max_heap and not visited[max_heap[0][1]]:
+            heapq.heappop(max_heap)
+        while min_heap and not visited[min_heap[0][1]]:
+            heapq.heappop(min_heap)
+    if not max_heap or not  min_heap:
         print('EMPTY')
     else:
-        print(arr[-1], arr[0])
+        print(-max_heap[0][0], min_heap[0][0])
