34e3f2994aa99313689ddf1de313dac11fa2f494
dhkimxx <dhkimxx@naver.com>
2022-04-01 21:12:32 +0900

    2022-04-01

--- 9252.py
+++ 9252.py
@@ -1,21 +1,17 @@
 A, B = input(), input()
 dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
-
 for i in range(1, len(A) + 1):
     for j in range(1, len(B) + 1):
         if A[i - 1] == B[j - 1]:
             dp[i][j] = dp[i - 1][j - 1] + 1
-            print(i, j, A[i - 1], B[j - 1])
         else:
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-LCS = []
+LCS = ''
 i = len(A)
 j = len(B)
-
 while i > 0 and j > 0:
     if dp[i][j - 1] == dp[i][j] - 1 and dp[i - 1][j] == dp[i][j] - 1:
-        LCS.append(A[i - 1])
+        LCS = A[i - 1] + LCS
         i -= 1
         j -= 1
     else:
@@ -23,6 +19,4 @@ while i > 0 and j > 0:
             j -= 1
         else:
             i -= 1
-
-print(dp[-1][-1])
-print(''.join(map(str, reversed(LCS))))
+print(dp[-1][-1], '\n', LCS)
