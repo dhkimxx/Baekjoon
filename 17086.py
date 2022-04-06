683ad5db4ecccb63a30be79a689cb058ca4cfe95
dhkimxx <dhkimxx@naver.com>
2022-04-06 17:26:16 +0900

    2022-04-06

--- 17086.py
+++ 17086.py
@@ -1,19 +1,28 @@
-import sys
 from collections import deque
 
-N, M = map(int, sys.stdin.readline().split())
+
+N, M = map(int, input().split())
 graph = []
+babyShark = []
 for n in range(N):
-    graph.append(list(map(int, sys.stdin.readline().split())))
-
-
-dx = [-1, 1, 0, 0, -1, 1, -1, 1]
-dy = [-1, 1, -1, 1, 1, -1, 0, 0]
-
-def bfs(start):
-    queue = deque([start])
-
-
-for n in range(N):
+    graph.append(list(map(int, input().split())))
     for m in range(M):
-        bfs()
+        if graph[n][m] == 1:
+            babyShark.append((n, m))
+d = [(-1, -1), (1, 1), (-1, 1), (1, -1), (0, 1), (0, -1), (-1, 0), (1, 0)]
+
+
+def bfs():
+    queue = deque(babyShark)
+    while queue:
+        x, y = queue.popleft()
+        for i in range(8):
+            nx, ny = x + d[i][0], y + d[i][1]
+            if nx < 0 or ny < 0 or nx >= N or ny >= M or graph[nx][ny] != 0:
+                continue
+            queue.append((nx, ny))
+            graph[nx][ny] = graph[x][y] + 1
+
+
+bfs()
+print(max(map(max, graph)) - 1)
