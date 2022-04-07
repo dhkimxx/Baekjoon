c5a8af5ed26f2c0c0a9597f3d0be0718430bcf56
dhkimxx <dhkimxx@naver.com>
2022-04-07 21:07:44 +0900

    2022-04-07

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+#define FastIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 using namespace std;
 
 int main() {
-    int A[4], sumA = 0;
-    int B[4], sumB = 0;
- 
-    for(int i = 0; i < 4; i++){
-        cin >> A[i];
-        sumA += A[i];
-    }
-    for(int i = 0; i < 4; i++){
-        cin >> B[i];
-        sumB += B[i];
-    }
-    cout << max(sumA, sumB);
+    FastIO;
 }
