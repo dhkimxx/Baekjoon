51477ea16dbdad3452aab055ecb2d7e77c88404c
dhkimxx <dhkimxx@naver.com>
2022-04-10 00:53:14 +0900

    2022-04-10

--- main.cpp
+++ main.cpp
@@ -2,11 +2,9 @@
 #define FastIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 using namespace std;
 
-int nextVertex[20001] = {0};
-int dp[20001] = {0};
-int visited[20001] = {0};
-vector<int> Stack;
-int Count = 0;
+bool visited[200000];
+int Count;
+int N;
 
 int numSum(int n){
     int numberSum = 0;
@@ -18,46 +16,24 @@ int numSum(int n){
 }
 
 int dfs(int v){
-    if(visited[v]) return
+    int nextVertex;
+    if(visited[v]) return 0;
     visited[v] = true;
-    Stack.push_back(v);
-    Count +=1;
-    if(nextVertex[v] != 0){
-        Count += dp[nextVertex[v]];
-        return 0;
-    }
-    dfs(nextVertex[v]);
-    return 0;
+    Count += 1;
+    nextVertex = v + numSum(v);
+    if(nextVertex > N) nextVertex -= N;
+    return dfs(nextVertex);
 }
 
 int main() {
     FastIO;
-    int N;
     int result = 0;
     cin >> N;
-    cout << N;
-    for(int i = 1; i <= N + 1; i++){
-        nextVertex[i] = i + numSum(i);
-        if(nextVertex[i] > N){
-            nextVertex[i] -= N;
-        }
-
-    }
-
-    for(int i = 0; i <= N + 1; i++){
-        if(dp[i] != 0) continue;
-        *visited = {0};
-        Stack.clear();
+    for(int i = 1; i <= N; i++){
+        for(int n = 1; n <= N; n++) visited[n] = false;
         Count = 0;
         dfs(i);
-        dp[i] = Count;
         result = result > Count ? result : Count;
-        for(int l = 1; l <= size(Stack) - 1; l++){
-            dp[Stack[l]] = Count - 1;
-        }
-
     }
-
-
     cout << result;
 }
