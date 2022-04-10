86442d2b8308a076665bbf81a07f1e0506a492c4
dhkimxx <dhkimxx@naver.com>
2022-04-10 14:33:59 +0900

    2022-04-10

--- main.cpp
+++ main.cpp
@@ -2,27 +2,53 @@
 #define FastIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 using namespace std;
 
-bool visited[200000];
-int Count;
+bool visited[200001];
+int dp[200001];
+vector<int> vec;
 int N;
+int cnt;
 
-int numSum(int n){
-    int numberSum = 0;
+int nextVertex(int n){
+    int sum = n;
     while(n != 0){
-        numberSum += n % 10;
+        sum += n % 10;
         n = n / 10;
     }
-    return numberSum;
+    if(sum > N) sum -= N;
+    return sum;
 }
 
 int dfs(int v){
-    int nextVertex;
-    if(visited[v]) return 0;
+    if(dp[v]){
+        int cicleSize = dp[v];
+        while (size(vec)){
+            dp[vec.back()] = ++cicleSize;
+            vec.pop_back();
+        }
+        return 0;
+    }
+    if(visited[v]){
+        vector<int> cicle;
+        int cicleSize = 0;
+        while(size(vec)){
+            cicleSize += 1;
+            cicle.push_back(vec.back());
+            vec.pop_back();
+            if(cicle.back() == v) break;
+        }
+        for(int i: cicle){
+            dp[i] = cicleSize;
+        }
+        while (size(vec)){
+            dp[vec.back()] = ++cicleSize;
+            vec.pop_back();
+        }
+        return 0;
+    }
     visited[v] = true;
-    Count += 1;
-    nextVertex = v + numSum(v);
-    if(nextVertex > N) nextVertex -= N;
-    return dfs(nextVertex);
+    cnt++;
+    vec.push_back(v);
+    return dfs(nextVertex(v));
 }
 
 int main() {
@@ -30,10 +56,11 @@ int main() {
     int result = 0;
     cin >> N;
     for(int i = 1; i <= N; i++){
-        for(int n = 1; n <= N; n++) visited[n] = false;
-        Count = 0;
+        if(visited[i]) continue;
+        vec.clear();
+        cnt = 0;
         dfs(i);
-        result = result > Count ? result : Count;
+        result = result > dp[i] ? result : dp[i];
     }
     cout << result;
 }
