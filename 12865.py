11bc09ad2828bf07cfa1f2e76b580862fc000ad4
dhkimxx <dhkimxx@naver.com>
2022-04-13 22:11:28 +0900

    2022-04-13

--- 12865.py
+++ 12865.py
@@ -1,33 +1,17 @@
-import copy
 N, K = map(int, input().split())
-weight = []
-value = []
-dp = [[]]
-
+weight = [0]
+value = [0]
+dp = [[0] * (K + 1) for _ in range(N + 1)]
 for i in range(N):
     W, V = map(int, input().split())
     weight.append(W)
     value.append(V)
-    dp[0].append(i)
 
-result = max(value)
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        if j >= weight[i]:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i])
+        else:
+            dp[i][j] = dp[i - 1][j]
 
-for i in range(1, N):
-    for j in range(0, N):
-        temp = copy.deepcopy(dp[i - 1])
-        if j not in temp:
-            continue
-        temp.remove(j)
-        dp.append(temp)
-
-print(dp)
-
-for W in dp:
-    SUM = 0
-    VAL = 0
-    for w in W:
-        SUM += weight[w]
-        VAL += value[w]
-    if SUM <= K:
-        result = max(VAL, result)
-print(result)
+print(dp[-1][-1])
