51560e3bf23e1c3294195244ec767f9b6c138e03
dhkim <dhkimxx@naver.com>
2022-04-15 10:27:17 +0900

    2022-04-15

--- main.cpp
+++ main.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main() {
     FastIO;
-
+    int T; cin >> T;
+    int N;
+    for(int i = 1; i  <= T; i++){
+        cin >> N;
+        int n = 0;
+        while(N > 0){
+            if(N % 2) cout << n << ' ';
+            N /= 2;
+            n++;
+        }
+        cout << '\n';
+    }
 }
