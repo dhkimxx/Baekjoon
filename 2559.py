6a52f57927085ad0a5be6e386e3713b71f890f04
dhkim <dhkimxx@naver.com>
2022-04-18 13:59:56 +0900

    2022-04-18

---
+++ 2559.py
@@ -0,0 +1,10 @@
+K, N = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+result = [0] * (K + 1)
+for i in range(1, K + 1):
+    arr[i] = arr[i - 1] + arr[i]
+max = -1e9
+for i in range(0, K - N + 1):
+    if max < arr[i + N] - arr[i]:
+        max = arr[i + N] - arr[i]
+print(max)
