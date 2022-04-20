0888eb2e5b2f66f60deb7c88682f50059df184dc
dhkimxx <dhkimxx@naver.com>
2022-04-20 17:26:21 +0900

    2022-04-20

--- 11660.py
+++ 11660.py
@@ -11,3 +11,19 @@ for i in range(0, N):
 for _ in range(M):
     x1, y1, x2, y2 = map(int, input().split())
     print(dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1])
+
+# import sys
+# input = sys.stdin.readline
+# N, M = map(int, input().split())
+# arr = [[0] * (N + 1)]
+# for i in range(1, N + 1):
+#     arr.append([0] + list(map(int, input().split())))
+# for i in range(1, N + 1):
+#     for j in range(1, N + 1):
+#         arr[i][j] = arr[i][j] + arr[i][j - 1]
+# for i in range(1, N + 1):
+#     for j in range(1, N + 1):
+#         arr[i][j] = arr[i][j] + arr[i - 1][j]
+# for _ in range(M):
+#     x1, y1, x2, y2 = map(int, input().split())
+#     print(arr[x2][y2] - arr[x2][y1 - 1] - arr[x1 - 1][y2] + arr[x1 - 1][y1 - 1])
