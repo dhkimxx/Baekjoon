0f4bbc3365e332f2337e4c771e68e037e09d021f
dhkimxx <dhkimxx@naver.com>
2022-04-23 23:05:47 +0900

    2022-04-23

---
+++ 1208.py
@@ -0,0 +1,53 @@
+import sys
+N, S = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+front = arr[:N//2]
+back = arr[N//2:]
+front_part_sum = []
+back_part_sum = []
+
+
+def find_part_sum(arr,  arr_part_sum, index, now_sum):
+    if index == len(arr):
+        arr_part_sum.append(now_sum)
+        return
+    find_part_sum(arr, arr_part_sum, index + 1, now_sum)
+    find_part_sum(arr, arr_part_sum, index + 1, now_sum + arr[index])
+
+
+find_part_sum(front, front_part_sum, 0, 0)
+find_part_sum(back, back_part_sum, 0, 0)
+front_part_sum = front_part_sum
+back_part_sum = back_part_sum
+back_part_sum.sort()
+print(front_part_sum, back_part_sum)
+cnt = 0
+for i in front_part_sum:
+    left = 0
+    right = len(back_part_sum) - 1
+
+    target = S - i
+    start = 0
+    end = len(back_part_sum) - 1
+    while end - start >= 0:
+        mid = (start + end) // 2
+        if back_part_sum[mid] <= target:
+            start = mid + 1
+            right = mid
+        if back_part_sum[mid] > target:
+            end = mid - 1
+
+    start = 0
+    end = len(back_part_sum) - 1
+    while end - start >= 0:
+        mid = (start + end) // 2
+        if back_part_sum[mid] < target:
+            start = mid + 1
+        if back_part_sum[mid] >= target:
+            end = mid - 1
+            left = mid
+    cnt += right - left + 1
+    print(left, right)
+if S == 0:
+    cnt -= 1
+print(cnt)
