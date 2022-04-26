b681eaa12f7a8fa2e6879cb6b982e5cf2a9d7404
dhkim <dhkimxx@naver.com>
2022-04-26 13:55:41 +0900

    2022-04-26

--- 11401.py
+++ 11401.py
@@ -1,8 +1,19 @@
 N, K = map(int, input().split())
 P = 1000000007
-factorial = [1] * (N + 1)
-for i in range(1, N + 1):
-    factorial[i] = i * factorial[i - 1] % P
-A = factorial[N] % P
-B = (factorial[K] * factorial[N - K]) % P
-print(A % B)
+dp = [1] * (N + 1)
+for i in range(2, N + 1):
+    dp[i] = i * dp[i - 1] % P
+
+
+def pow(A, B):
+    if B == 1:
+        return A % P
+    if B % 2 == 0:
+        return pow(A, B//2) ** 2 % P
+    if B % 2 == 1:
+        return A * (pow(A, B//2) ** 2) % P
+
+
+a = dp[N]
+b = dp[N - K] * dp[K] % P
+print(a * pow(b, P - 2) % P)
