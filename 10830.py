c1770c72e7111069d54810a4d4b08bf7cddeb4d7
dhkimxx <dhkimxx@naver.com>
2022-04-28 22:58:06 +0900

    2022-04-28

--- 10830.py
+++ 10830.py
@@ -1,16 +1,26 @@
-import copy
 N, M = map(int, input().split())
-matrix1 = [list(map(int, input().split())) for _ in range(N)]
-matrix2 = copy.deepcopy(matrix1)
+matrix = [list(map(int, input().split())) for _ in range(N)]
 
-for m in range(M-1):
-    matrix = [[0] * N for _ in range(N)]
+def mul(A, B):
+    temp_matrix = [[0] * N for _ in range(N)]
     for n in range(N):
         for k in range(N):
             for m in range(N):
-                matrix[n][k] += matrix1[n][m] * matrix2[m][k]
-                matrix[n][k] %= 1000
-    matrix1 = copy.deepcopy(matrix)
+                temp_matrix[n][k] += A[n][m] * B[m][k]
+                temp_matrix[n][k] %= 1000
+    return temp_matrix
 
-for i in range(N):
-    print(*matrix1[i])
+def divide(B):
+    if B == 1:
+        return matrix
+    elif B % 2 == 0:
+        result = divide(B//2)
+        return mul(result, result)
+    elif B % 2 == 1:
+        result = divide(B//2)
+        return mul(mul(result, result), matrix)
+
+for result in divide(M):
+    for r in result:
+        print(r % 1000, end=' ')
+    print()
