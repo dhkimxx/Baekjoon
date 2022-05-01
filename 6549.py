4f8d3afa7883de4a689d68df87fe553caeb048a1
dhkimxx <dhkimxx@naver.com>
2022-05-01 21:49:48 +0900

    2022-05-01

--- 6549.py
+++ 6549.py
@@ -1,18 +1,18 @@
 import sys
-from collections import deque
+
 while 1:
-    arr = list(map(int, sys.stdin.readline().split()))
-    if arr[0] == 0:
-        break
-    stack = deque()
-    max_size = 0
-    last_size = 0
-    for i in range(1, arr[0] + 1):
-        stack.append(arr[i])
-        if last_size < len(stack) * min(stack):
-            stack = deque([])
-        if len(stack) * min(stack) < last_size:
-            max_size = last_size
-        last_size = len(stack) * min(stack)
-        print('len:', len(stack), 'size:', len(stack) * min(stack), stack)
-    print(arr[1:])
+    n, *arr = list(map(int, sys.stdin.readline().split()))
+    if n == 0: break
+    stack = []
+    ans = 0
+    arr.append(0)
+    for i, h in enumerate(arr):
+        while stack and arr[stack[-1]] > h:
+            height = arr[stack.pop()]
+            if stack:
+                width = i - stack[-1] -1
+            else:
+                width = i
+            ans = max(ans, height * width)
+        stack.append(i)
+    print(ans)
