46415f604a6d471e33f1050835b6c9ccc82fe3f7
dhkimxx <dhkimxx@naver.com>
2022-05-15 20:04:16 +0900

    2022-05-15

--- 2477.py
+++ 2477.py
@@ -1,6 +1,23 @@
 K = int(input())
-arr = []
+length = []
+direction = []
+max_width = 0
+max_height = 0
 for i in range(6):
-    direction, length = map(int, input().split())
-    arr.append(length)
+    d, l = map(int, input().split())
+    length.append(l)
+    direction.append(d)
+    if d == 1 or d == 2:
+        if max_width < l:
+            max_width = l
+            max_width_index = i
+    if d == 3 or d == 4:
+        if max_height < l:
+            max_height = l
+            max_height_index = i
 
+big_box = max_width * max_height
+small_box_height = abs(length[(max_width_index - 1) % 6] - length[(max_width_index + 1) % 6])
+small_box_width = abs(length[(max_height_index + 1) % 6] - length[(max_height_index - 1) % 6])
+small_box = small_box_width * small_box_height
+print(K * (big_box - small_box))
