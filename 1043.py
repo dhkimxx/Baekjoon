811c92d73de99d90a10705c096efd3cdbb8cf61b
dhkim <dhkimxx@naver.com>
2022-05-18 23:12:40 +0900

    2022-05-18

--- 1043.py
+++ 1043.py
@@ -2,7 +2,7 @@ from collections import deque
 
 N, M = map(int, input().split())
 K_n, *K = list(map(int, input().split()))
-visited = [0] * (N + 1)
+checked_person = [0] * (N + 1)
 able_party = [1] * M
 graph = []
 for _ in range(M):
@@ -11,14 +11,14 @@ for _ in range(M):
 
 q = deque(K)
 for k in K:
-    visited[k] = 1
+    checked_person[k] = 1
 while q:
     v = q.popleft()
     for m in range(M):
         if v in graph[m]:
             able_party[m] = 0
             for nv in graph[m]:
-                if not visited[nv]:
+                if not checked_person[nv]:
                     q.append(nv)
-                    visited[nv] = 1
+                    checked_person[nv] = 1
 print(sum(able_party))
