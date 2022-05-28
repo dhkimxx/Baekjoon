c6ed8f9dabe5d02f84d2831bddfcfe1858b517c4
dhkimxx <dhkimxx@naver.com>
2022-05-28 18:46:04 +0900

    2022-05-28

--- 11438.py
+++ 11438.py
@@ -1,12 +1,12 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
-LOG = 21 # 2^20 = 1,000,000
+LOG = 21    # 2^20 = 1,000,000
 
 N = int(input())
 graph = [[] for _ in range(N + 1)]
 depth = [0] * (N + 1)
-parent = [0] * (N + 1)
+parent = [[0] * LOG for _ in range(N + 1)]
 for _ in range(N-1):
     a, b = map(int, input().split())
     graph[a].append(b)
@@ -20,23 +20,31 @@ def bfs(start):
         for nv in graph[v]:
             if not depth[nv]:
                 depth[nv] = depth[v] + 1
-                parent[nv] = v
+                parent[nv][0] = v
                 q.append(nv)
 
-def lca(a, b):
-    while depth[a] != depth[b]:
-        if depth[a] > depth[b]:
-            a = parent[a]
-        else:
-            b = parent[b]
+def set_parent():
+    bfs(1)
+    for i in range(1, LOG):
+        for j in range(1, N + 1):
+            parent[j][i] = parent[parent[j][i-1]][i-1]
 
-    while a != b:
-        a = parent[a]
-        b = parent[b]
+def lca(a, b):
+    if depth[a] > depth[b]:
+        a, b = b, a
+    for i in range(LOG - 1, -1, -1):
+        if depth[b] - depth[a] >= (1 << i):
+            b = parent[b][i]
+    if a == b:
+        return a
+    for i in range(LOG - 1, -1, -1):
+        if parent[a][i] != parent[b][i]:
+            a = parent[a][i]
+            b = parent[b][i]
     return a
 
 
-bfs(1)
+set_parent()
 M = int(input())
 for _ in range(M):
     u, v = map(int, input().split())
--- main.py
+++ main.py
@@ -1,54 +1,64 @@
-import heapq
 from collections import deque
+import heapq
 import sys
 input = sys.stdin.readline
+LOG = 22    # 2^21 = 2,000,000
 
 N, M = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
 input_lines = []
 for _ in range(M):
     u, v, w = map(int, input().split())
-    if u > v:
-        u, v = v, u
     input_lines.append((u, v, w))
     graph[u].append((w, v))
     graph[v].append((w, u))
 
-visited = [False] * (N + 1)
-mst = [[] for _ in range(N + 1)]
+
+depth = [0] * (N + 1)
+parent = [[0] * LOG for _ in range(N + 1)]
 sum_weight = 0
+max_weight = 0
+LOG_weight = [[0] * LOG for _ in range(N + 1)]
 q = [(0, 1, 0)]
 cnt = 0
 
 while q and cnt < N:
     weight, now, last = heapq.heappop(q)
-    if not visited[now]:
-        visited[now] = True
-        mst[now].append((last, weight))
-        mst[last].append((now, weight))
-        sum_weight += weight
+    sum_weight += weight
+    if not depth[now]:
+        depth[now] = depth[last] + 1
         cnt += 1
         for next in graph[now]:
             heapq.heappush(q, (next[0], next[1], now))
+            parent[next[1]][0] = now
+            max_weight = max(max_weight, next[0])
+            LOG_weight[next[1]][0] = max_weight
 
+for i in range(1, LOG):
+    for j in range(1, N + 1):
+        parent[j][i] = parent[parent[j][i-1]][i-1]
+        LOG_weight[j][i] = max(LOG_weight[j][i-1], LOG_weight[parent[j][i-1]][i-1])
 
-def bfs(start, target):
-    q = deque([(start, 0)])
-    visited[start] = 1
-    while q:
-        now, max_weight = q.popleft()
-        for next in mst[now]:
-            if not visited[next[0]]:
-                q.append((next[0], max(max_weight, next[1])))
-                visited[next[0]] = visited[now] + 1
-                if target == next[0]:
-                    return max(max_weight, next[1]), visited[next[0]]
+def lca(a, b):
+    if depth[a] > depth[b]:
+        a, b = b, a
+    maxW = max(LOG_weight[a][LOG - 1], LOG_weight[b][LOG - 1])
+    for i in range(LOG - 1, -1, -1):
+        if depth[b] - depth[a] >= (1 << i):
+            b = parent[b][i]
+            maxW = max(maxW, LOG_weight[b][i])
+    if a == b:
+        return maxW
+
+    for i in range(LOG - 1, -1, -1):
+        if parent[a][i] != parent[b][i]:
+            a = parent[a][i]
+            b = parent[b][i]
+            maxW = max(maxW, LOG_weight[b][i], LOG_weight[b][i])
+    return maxW
 
 
 for u, v, w in input_lines:
-    visited = [0] * (N + 1)
-    loop_max_weight, count = bfs(u, v)
-    if count == 2:
-        print(sum_weight)
-    else:
-        print(sum_weight - loop_max_weight + w)
+    temp = lca(u, v)
+    print(temp)
+    print(max(sum_weight, sum_weight - temp + w))
