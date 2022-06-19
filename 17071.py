f0ec8193ca08acfd0e85111b7b7d136250385284
dhkimxx <dhkimxx@naver.com>
2022-06-19 23:41:05 +0900

    2022-06-19

--- 17071.py
+++ 17071.py
@@ -1,19 +1,41 @@
 from collections import deque
-N, K = map(int, input().split())
-INF = 500001
-visited = [0] * INF
-def bfs():
-    q = deque([(N, K, 0)])
-    visited[N] = 1
-    while q:
-        v, target, timer = q.popleft()
-        if v == target:
-            return timer
-        for nv in [v - 1, v + 1, 2 * v]:
-            next_timer = timer + 1
-            next_target = target + next_timer
-            if 0 <= nv < INF and 0 <= next_target < INF and not visited[nv]:
-                visited[nv] = 1
 
-    return -1
-print(bfs())
+N, K = map(int, input().split())
+if N == K:
+    print(0)
+    exit()
+INF = 500000
+visited = [[0] * 2 for _ in range(INF + 1)]
+ok = False
+turn = 1
+q = deque([N])
+visited[N][0] = 1
+while q:
+    K += turn
+    if K > INF:
+        break
+    if visited[K][turn % 2]:
+        ok = True
+        break
+    ThisTurnSize = len(q)
+    for j in range(ThisTurnSize):
+        dist = q.popleft()
+        for nx in [dist + 1, dist - 1, dist * 2]:
+            if nx < 0 or nx > INF:
+                continue
+            if nx == K:
+                ok = True
+                break
+            if visited[nx][turn % 2]:
+                continue
+            visited[nx][turn % 2] = 1
+            q.append(nx)
+        if ok:
+            break
+    if ok:
+        break
+    turn += 1
+if ok:
+    print(turn)
+else:
+    print(-1)
