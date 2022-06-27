cc62f99b2211f204dd7408aab4710df74617ad28
dhkimxx <dhkimxx@naver.com>
2022-06-27 21:28:46 +0900

    2022-06-27

--- 17071.py
+++ 17071.py
@@ -1,37 +1,22 @@
 from collections import deque
 
 N, K = map(int, input().split())
-if N == K:
-    print(0)
-    exit()
-INF = 500000
-visited = [[0] * 2 for _ in range(INF + 1)]
-flag = False
-turn = 1
-q = deque([N])
-visited[N][0] = 1
-while 1:
-    K += turn
-    if K > INF:
-        break
-    if visited[K][turn % 2]:
-        flag = True
-        break
-    for j in range(len(q)):
-        v = q.popleft()
-        for nv in [v + 1, v - 1, v * 2]:
-            if 0 <= nv <= INF and not visited[nv][turn % 2]:
-                visited[nv][turn % 2] = 1
-                q.append(nv)
-                if nv == K:
-                    flag = True
-                    break
-        if flag:
-            break
-    if flag:
-        break
-    turn += 1
-if flag:
-    print(turn)
-else:
-    print(-1)
+visited = [[1e9, 1e9] for _ in range(500001)]
+q = deque([(N, 0)])
+visited[N][0] = 0
+while q:
+    v, t = q.popleft()
+    for nv in [v + 1, v - 1, 2 * v]:
+        if 0 <= nv <= 500000 and visited[nv][(t + 1) % 2] == 1e9:
+            visited[nv][(t + 1) % 2] = visited[v][t % 2] + 1
+            q.append((nv, t + 1))
+
+time = 0
+while K <= 500000:
+    if visited[K][time % 2] != 1e9:
+        if visited[K][time % 2] <= time:
+            print(time)
+            exit()
+    time += 1
+    K += time
+print(-1)
