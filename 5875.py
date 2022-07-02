d153af81ce2465d1c1c4f74df1cf2c1f1bdcfd44
dhkimxx <dhkimxx@naver.com>
2022-07-02 18:46:08 +0900

    2022-07-02

--- 5875.py
+++ 5875.py
@@ -1,30 +1,19 @@
-import sys
-
-S = list(sys.stdin.readline().rstrip())
+S = list(input())
 left = 0
 right = 0
 Sum = 0
-
 for n in range(len(S)):
     if S[n] == '(':
         Sum += 1
         left += 1
-        if n == len(S) - 1:
-            print(1)
-            sys.exit()
-        if Sum == 2 and n != len(S) - 1:
-            left = 1
-
-    if S[n] == ')':
+    else:
         Sum -= 1
         right += 1
-        if n == 0:
-            print(1)
-            sys.exit()
-        if Sum == -2:
-            print(right - 1)
-            sys.exit()
-
+    if Sum < 2:
+        left = 0
+    if Sum == -1:
+        print(right)
+        exit()
 if Sum == 0:
     print(0)
 else:
