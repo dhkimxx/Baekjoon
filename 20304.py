1dbdd3dfec8d1552189ff0a31a37e435d48f6e9a
dhkimxx <dhkimxx@naver.com>
2022-07-13 17:05:01 +0900

    2022-07-13

--- 20304.py
+++ 20304.py
@@ -1,12 +1,12 @@
+from collections import deque
+
 N, M = int(input()), int(input())
 P = list(map(int, input().split()))
-passward = [0] * (N + 1)
-for p in P:
-    passward[p] = 1
-graph = [[1e9] * (N + 1) for _ in range(N + 1)]
-
+ans = 0
 for i in range(0, N + 1):
-    for j in range(i + 1, N + 1):
-        if passward[i] and passward[j]:
-            graph[i][j] = sum(list(map(int, bin(i ^ j)[2:])))
-            print(graph[i][j])
+    score = 0
+    for p in P:
+        xor_sum = sum(list(map(int, bin(p ^ i)[2:])))
+        score = min(score, xor_sum)
+    ans = max(score, ans)
+print(ans)
