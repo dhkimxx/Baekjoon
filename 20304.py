55544902b2c7bc0486e5c5fff8c0503fe7451c82
dhkimxx <dhkimxx@naver.com>
2022-08-22 22:43:12 +0900

    2022-08-22

--- 20304.py
+++ 20304.py
@@ -1,24 +1,16 @@
 from collections import deque
-
-N = int(input())
-M = int(input())
+N, M = int(input()), int(input())
 P = list(map(int, input().split()))
-
-safety = [21 for _ in range(N + 1)]
+safety = [21] * (N + 1)
 q = deque()
-
 for p in P:
     safety[p] = 0
     q.append(p)
-
-ans = 0
-
 while q:
     cur = q.popleft()
     for i in range(20):
         x = (1 << i) ^ cur
         if x <= N and safety[cur] + 1 < safety[x]:
             safety[x] = safety[cur] + 1
-            ans = max(safety[x], ans)
             q.append(x)
-print(ans)
+print(max(safety))
