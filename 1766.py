262dd7c4d7ec1c99e76585240e2a430a96d11201
dhkimxx <dhkimxx@naver.com>
2022-11-21 05:30:21 +0900

    2022-11-21

--- 1766.py
+++ 1766.py
@@ -1,24 +1,25 @@
-from collections import deque
+import heapq
 
 N, M = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
 inDegree = [0] * (N + 1)
 for i in range(M):
     a, b = map(int, input().split())
-    graph[b].append(a)
-    inDegree[a] += 1
+    graph[a].append(b)
+    inDegree[b] += 1
 
-q = deque([])
-for i in range(1, N  + 1):
+heap = []
+for i in range(1, N + 1):
     if inDegree[i] == 0:
-        q.append(i)
+        heapq.heappush(heap, i)
 
 ans = []
-while q:
-    now = q.popleft()
+while heap:
+    now = heapq.heappop(heap)
     ans.append(now)
     for next in graph[now]:
         inDegree[next] -= 1
         if inDegree[next] == 0:
-            q.append(next)
-print(len(ans))
+            heapq.heappush(heap, next)
+
+print(*ans)
