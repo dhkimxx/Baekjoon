9af00c2e9fd322bd2f9e3bbdf0105a43e6b9b7eb
dhkimxx <dhkimxx@naver.com>
2023-02-14 22:15:06 +0900

    2023-02-14

---
+++ 1182.py
@@ -0,0 +1,15 @@
+def dfs(level, sum_sub):
+    global cnt
+    if level >= N:
+        return
+    if sum_sub + arr[level] == S:
+        cnt += 1
+    dfs(level + 1, sum_sub + arr[level]) #level번째 원소를 부분수열에 포함
+    dfs(level + 1, sum_sub)              #level번쨰 원소를 부분수열에 미포함
+
+
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+cnt = 0
+dfs(0, 0)
+print(cnt)
