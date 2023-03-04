2210a847aa34f03f57af8f32ab07087fda733103
kimdohyeong <dhkimxx@naver.com>
2023-03-04 23:36:27 +0900

    2023-03-04

--- 14675.py
+++ 14675.py
@@ -1,54 +1,16 @@
-from collections import deque
-
-
-def tree(t, k):
-    if t == 1:
-        if k + 1 <= N:
-            start = k + 1
-        else:
-            start = k - 1
-        q = deque([start])
-        visited[start] = 1
-        while q:
-            now = q.popleft()
-            for i, _ in graph[now]:
-                if i != k and not visited[i]:
-                    visited[i] = 1
-                    q.append(i)
-
-        if sum(visited) == N - 1:
-            return 'no'
-        else:
-            return 'yes'
-
-    if t == 2:
-        q = deque([start])
-        visited[start] = 1
-        while q:
-            now = q.popleft()
-            for i, _ in graph[now]:
-                if i != k and not visited[i]:
-                    visited[i] = 1
-                    q.append(i)
-
-        if sum(visited) == N - 1:
-            return 'no'
-        else:
-            return 'yes'
-
-
-
+import sys
+input = sys.stdin.readline
 N = int(input())
-graph = [[] for _ in range(N + 1)]
-edge = []
+graph = [0] * (N + 1)
+
 for n in range(N - 1):
     a, b = map(int, input().split())
-    graph[a].append([b, n])
-    graph[b].append([a, n])
+    graph[a] += 1
+    graph[b] += 1
 
 for q in range(int(input())):
-    visited = [0] * (N + 1)
     t, k = map(int, input().split())
-    print(tree(t, k))
-
-
+    res = 'yes'
+    if t == 1 and graph[k] == 1:
+        res = 'no'
+    print(res)
