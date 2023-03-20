984ccf5dab6f4ef0f10ac677b0c03bd8d8312b82
kimdohyeong <dhkimxx@naver.com>
2023-03-20 14:04:36 +0900

    2023-03-20

--- 2240.py
+++ 2240.py
@@ -1,13 +1,12 @@
 T, W = map(int, input().split())
 tree = [int(input()) for _ in range(T)]
-dp = [[[0] * 3 for _ in range(W + 1)] for _ in range(T + 1)]
-for t in range(1, T + 1):
-    if tree[t - 1] == 1:
-        dp[t][0][1] = dp[t - 1][0][1] + 1
-    else:
-        dp[t][0][2] = dp[t - 1][0][2] + 1
-
-    for w in range(1, W + 1):
+dp = [[[0] * 3 for _ in range(W + 2)] for _ in range(T + 1)]
+if tree[0] == 1:
+    dp[1][0][1] = 1
+else:
+    dp[1][1][2] = 1
+for t in range(2, T + 1):
+    for w in range(W + 1):
         if tree[t - 1] == 1:
             dp[t][w][1] = max(dp[t - 1][w - 1][2] + 1, dp[t - 1][w][1] + 1)
             dp[t][w][2] = max(dp[t - 1][w - 1][1], dp[t - 1][w][2])
@@ -15,3 +14,5 @@ for t in range(1, T + 1):
             dp[t][w][1] = max(dp[t - 1][w - 1][2], dp[t - 1][w][1])
             dp[t][w][2] = max(dp[t - 1][w - 1][1] + 1, dp[t - 1][w][2] + 1)
 print(max(max(map(max, dp))))
+for i in range(T + 1):
+    print(dp[i])
