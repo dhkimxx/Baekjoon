98fc940ac02b8ed67214e97bf4895987984a146a
kimdohyeong <dhkimxx@naver.com>
2023-04-05 13:55:23 +0900

    2023-04-05

--- 2745.py
+++ 2745.py
@@ -2,7 +2,7 @@ N, B = input().split()
 N = ''.join(reversed(N))
 B = int(B)
 
-number = '0123456789ABCDEFGHIJKLNMOPQRSTUVWXYZ'
+number = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 ans = 0
 for n in range(len(N)):
     ans += number.index(N[n]) * (B ** n)
